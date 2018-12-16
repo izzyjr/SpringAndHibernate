4d277f1c898bf6a1a079cafecf07276fe0ca97ab
Israel Mesa <mesa.israel50@gmail.com>
2018-12-16 10:27:56 -0500

    New Bean

---
+++ spring-demo-one/src/com/mesa/springdemo/SoccerCoach.java
@@ -0,0 +1,10 @@
+package com.mesa.springdemo;
+
+public class SoccerCoach implements Coach{
+
+	@Override
+	public String getDailyWorkout() {
+		return "Practice penalty shooting for 30 minutes";
+	}
+	
+}
