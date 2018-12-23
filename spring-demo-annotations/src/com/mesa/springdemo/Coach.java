99b50d587d0d3e4944b0c0659a8bc3307fcd63ba
Israel Mesa <mesa.israel50@gmail.com>
2018-12-23 12:03:38 -0500

    Spring Configuration with Java Annotations - Constructor Injection

--- spring-demo-annotations/src/com/mesa/springdemo/Coach.java
+++ spring-demo-annotations/src/com/mesa/springdemo/Coach.java
@@ -3,5 +3,7 @@ package com.mesa.springdemo;
 public interface Coach {
 
 	public String getDailyWorkout();
+	public String getDailyFortune();
+	
 	
 }
--- spring-demo-annotations/src/com/mesa/springdemo/SoccerCoach.java
+++ spring-demo-annotations/src/com/mesa/springdemo/SoccerCoach.java
@@ -1,13 +1,27 @@
 package com.mesa.springdemo;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 @Component("CholoSimeone")
 public class SoccerCoach implements Coach {
 
+	private FortuneService fortuneService;
+	
+	
+	@Autowired
+	public SoccerCoach(FortuneService fortuneService) {
+		this.fortuneService = fortuneService;
+	}
+
 	@Override
 	public String getDailyWorkout() {
-		return "Practice corner kicks for 30 minutes";
+		return "Practice corner kicks";
+	}
+
+	@Override
+	public String getDailyFortune() {
+		return fortuneService.getFortune();
 	}
 
 }
