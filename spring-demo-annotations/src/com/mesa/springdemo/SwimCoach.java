074c951ba8fe51887092395c67baad4d15ee84f9
Israel Mesa <mesa.israel50@gmail.com>
2018-12-26 13:04:40 -0500

    How to inject properties file using Java annotations

---
+++ spring-demo-annotations/src/com/mesa/springdemo/SwimCoach.java
@@ -0,0 +1,43 @@
+package com.mesa.springdemo;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+@Component
+public class SwimCoach implements Coach {
+	
+	@Autowired
+	@Qualifier("randomFortuneService")
+	private FortuneService fortuneService;
+	
+	@Value("${foo.email}")
+	private String email;
+	    
+	@Value("${foo.team}")
+	private String team;
+	
+	public SwimCoach() {
+		System.out.println(">> SwimCoach: Inside the default constructor!");
+	}
+
+	@Override
+	public String getDailyWorkout() {
+		return "100 laps of Butterfly stroke";
+	}
+
+	@Override
+	public String getDailyFortune() {
+		return fortuneService.getFortune();
+	}
+	
+	public String getEmail() {
+		return email;
+	}
+
+	public String getTeam() {
+		return team;
+	}
+
+}
