a296f87927ed3d5a67f3f5a6b782576a2804a090
Israel Mesa <mesa.israel50@gmail.com>
2018-12-27 13:20:24 -0500

    + extra interface implementation - @Qualifier Annotation practice

--- spring-understanding-annotations/src/com/mesa/springdemo/SwimCoach.java
+++ spring-understanding-annotations/src/com/mesa/springdemo/SwimCoach.java
@@ -1,22 +1,31 @@
 package com.mesa.springdemo;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Component;
 
 @Component
 public class SwimCoach implements Coach {
 	
+	/*
+	@Autowired
+	@Qualifier("happyFortuneService")
+	*/
 	private FortuneService fortuneService;
+	
 	
 	// Constructor Injection
 	@Autowired
-	public SwimCoach(FortuneService fortuneService) {
+	public SwimCoach(@Qualifier("propertiesFileFortunes") FortuneService fortuneService) {
+		System.out.println(">> SwimCoach: Inside the default constructor");
 		this.fortuneService = fortuneService;
 	}
 	
+	/*
 	public SwimCoach() {
 		System.out.println(">> SwimCoach: Inside the default constructor");
 	}
+	*/
 
 	@Override
 	public String getDailyWorkout() {
