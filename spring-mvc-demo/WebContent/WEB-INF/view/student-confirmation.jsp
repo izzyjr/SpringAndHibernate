de2ebb8b03845ab05926c2e3a52d33123d69d2bf
Israel Mesa <mesa.israel50@gmail.com>
2019-01-03 13:33:27 -0500

    Spring MVC - Form Tags - Drop Down Lists

--- spring-mvc-demo/WebContent/WEB-INF/view/student-confirmation.jsp
+++ spring-mvc-demo/WebContent/WEB-INF/view/student-confirmation.jsp
@@ -9,6 +9,10 @@
 <body>
 
 	The student is confirmed: ${student.firstName} ${student.lastName}
+	
+	<br><br>
+	
+	Country: ${student.country}
 
 </body>
 
--- spring-mvc-demo/src/com/mesa/springdemo/mvc/StudentController.java
+++ spring-mvc-demo/src/com/mesa/springdemo/mvc/StudentController.java
@@ -25,7 +25,7 @@ public class StudentController {
 	public String processForm(@ModelAttribute("student") Student theStudent) {
 		
 		// log the input data
-		System.out.println("theStudent: " + theStudent.getFirstName() + " " + theStudent.getLastName());
+		System.out.println("theStudent: " + theStudent.getFirstName() + " " + theStudent.getLastName() + " - " + theStudent.getCountry());
 
 		return "student-confirmation";
 	}
