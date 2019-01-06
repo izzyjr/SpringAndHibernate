97917c1aa33a50a12f09f3d36d3e7cb6a10c4420
Israel Mesa <mesa.israel50@gmail.com>
2019-01-06 12:29:45 -0500

    Creating Custom Annotation - 2

--- spring-mvc-demo/src/com/mesa/springdemo/mvc/Customer.java
+++ spring-mvc-demo/src/com/mesa/springdemo/mvc/Customer.java
@@ -6,6 +6,8 @@ import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Pattern;
 import javax.validation.constraints.Size;
 
+import com.mesa.springdemo.mvc.validation.CourseCode;
+
 public class Customer {
 	
 	private String firstName;
@@ -20,8 +22,11 @@ public class Customer {
 	@Max(value = 10, message = "must be less than or equal to 10")
 	private Integer freePasses;
 	
-	@Pattern(regexp="^[a-zA-Z0-9]{5}", message="only 5 chars/digits")
+	@Pattern(regexp="^[a-zA-Z0-9]{5}", message = "only 5 chars/digits")
 	private String postalCode;
+	
+	@CourseCode(value = "TOPS", message = "must start with TOPS")
+	private String courseCode;
 
 	public Customer() {
 	
@@ -57,6 +62,14 @@ public class Customer {
 
 	public void setPostalCode(String postalCode) {
 		this.postalCode = postalCode;
+	}
+
+	public String getCourseCode() {
+		return courseCode;
+	}
+
+	public void setCourseCode(String courseCode) {
+		this.courseCode = courseCode;
 	}
 	
 }
