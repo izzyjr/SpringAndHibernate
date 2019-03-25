57727095156e1dcbce1651c0203c6c1685c00028
Israel Mesa <mesa.israel50@gmail.com>
2019-03-25 16:03:11 -0400

    DAO Interface and its implementation class

---
+++ web-customer-tracker/src/com/mesa/springdemo/dao/CustomerDAO.java
@@ -0,0 +1,11 @@
+package com.mesa.springdemo.dao;
+
+import java.util.List;
+
+import com.mesa.springdemo.entity.Customer;
+
+public interface CustomerDAO {
+	
+	public List<Customer> getCustomers();
+
+}
