a222220af8903c18fc5b675e178760938cb68a23
Israel Mesa <mesa.israel50@gmail.com>
2019-03-31 13:45:37 -0400

    GetMapping Annotation

--- SpringHibernate-YahooFinanceScraper/src/com/mesa/springscraper/controller/CryptoController.java
+++ SpringHibernate-YahooFinanceScraper/src/com/mesa/springscraper/controller/CryptoController.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.mesa.springscraper.dao.CoinDAO;
@@ -18,7 +19,7 @@ public class CryptoController {
 	@Autowired
 	private CoinDAO coinDAO;
 
-	@RequestMapping("/list")
+	@GetMapping("/list")
 	public String listCoins(Model theModel) {
 		
 		// get coins from the DAO
