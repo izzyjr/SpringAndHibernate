4f0a01b24b9569aa1d4becd81cf8c9163459bf80
Israel Mesa <mesa.israel50@gmail.com>
2019-04-10 20:13:58 -0400

    DI Constructor

---
+++ spring-understanding-annotations/src/com/mesa/springdemo/TrackCoach.java
@@ -0,0 +1,27 @@
+package com.mesa.springdemo;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.stereotype.Component;
+
+@Component
+public class TrackCoach implements Coach {
+	
+	private FortuneService fortuneService;
+
+	@Autowired
+	public TrackCoach(@Qualifier("happyFortuneService")FortuneService fortuneService) {
+		this.fortuneService = fortuneService;
+	}
+
+	@Override
+	public String getDailyWorkout() {
+		return "Run 5 Miles";
+	}
+
+	@Override
+	public String getDailyFortune() {
+		return fortuneService.getFortune();
+	}
+
+}
