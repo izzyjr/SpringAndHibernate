0d0be75268a4bb9958a53a4d9f9024ac4b16c549
Israel Mesa <mesa.israel50@gmail.com>
2019-10-02 19:58:44 -0400

    Exception Handling

--- spring-rest-demo/src/main/java/com/mesa/springdemo/rest/StudentRestController.java
+++ spring-rest-demo/src/main/java/com/mesa/springdemo/rest/StudentRestController.java
@@ -5,6 +5,9 @@ import java.util.List;
 
 import javax.annotation.PostConstruct;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -38,8 +41,52 @@ public class StudentRestController {
 	public Student getStudent(@PathVariable int studentId) {
 		
 		// index into the list
+		if(studentId >= theStudents.size() || studentId < 0) {
+			throw new StudentNotFoundException("Student id not found - " + studentId);
+		}
+		
+		// check student id against list size
 		return theStudents.get(studentId);
 	}
 	
+	// Add an execption handler using @ExceptionHandler 
+	@ExceptionHandler
+	public ResponseEntity<StudentErrorResponse> handleException(StudentNotFoundException exc) {
+		
+		// create a student error response
+		StudentErrorResponse error = new StudentErrorResponse();
+		error.setStatus(HttpStatus.NOT_FOUND.value());
+		error.setMessage(exc.getMessage());
+		error.setTimeStamp(System.currentTimeMillis());
+		
+		// return ResponseEntity	
+		return new ResponseEntity<>(error, HttpStatus.NOT_FOUND);
+	}
 
+	// Add another exception handler to catch any exception
+	@ExceptionHandler 
+	public ResponseEntity<StudentErrorResponse> handleException(Exception exc) {
+		
+		// create a student error response
+		StudentErrorResponse error = new StudentErrorResponse();
+		error.setStatus(HttpStatus.BAD_REQUEST.value());
+		error.setMessage(exc.getMessage());
+		error.setTimeStamp(System.currentTimeMillis());
+				
+		// return ResponseEntity	
+		return new ResponseEntity<>(error, HttpStatus.BAD_REQUEST);
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
 }
