01026731cae0802734feafba9fb89dd7730cda3a
Israel Mesa <mesa.israel50@gmail.com>
2019-10-12 17:36:45 -0400

    create/post, update/put

--- spring-crm-rest-demo/src/main/java/com/luv2code/springdemo/rest/CustomerRestController.java
+++ spring-crm-rest-demo/src/main/java/com/luv2code/springdemo/rest/CustomerRestController.java
@@ -5,6 +5,9 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -38,6 +41,29 @@ public class CustomerRestController {
 		
 		return theCustomer;
 	}
+	
+	// add mapping for POST /customers - add new customer	
+	@PostMapping("/customers")
+	public Customer addCustomer(@RequestBody Customer theCustomer) {
+		
+		//just in case they pass an id in JSON ... set id to 0
+		//this will force a new save as opposed to an update
+		theCustomer.setId(0);
+		
+		customerService.saveCustomer(theCustomer);
+		
+		return theCustomer; 
+	}
+	
+	// add mapping for PUT /customers - update new customer
+	@PutMapping("/customers")
+	public Customer updateCustomer(@RequestBody Customer theCustomer) {
+		
+		customerService.saveCustomer(theCustomer);
+		
+		return theCustomer;
+	}
+	
 	
 
 }
